53c1a2975535d43e4b3b1471f0f8121ec219bfc8
zaech727 <131217133+zaech727@users.noreply.github.com>
2024-04-19 16:43:23 -0500

    fixed colors/improvements

--- frontend/app/login/page.jsx
+++ frontend/app/login/page.jsx
@@ -1,33 +1,54 @@
 'use client'
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navigation from '../components/nav';
-import './page.modules.css'
+import './page.modules.css';
 
 function LoginPage() {
+  // Hook to dynamically load Font Awesome
+  useEffect(() => {
+    const fontAwesomeScript = document.createElement('link');
+    fontAwesomeScript.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css';
+    fontAwesomeScript.rel = 'stylesheet';
+    document.head.appendChild(fontAwesomeScript);
+
+    return () => {
+      document.head.removeChild(fontAwesomeScript);
+    };
+  }, []);
+
   return (
-    <div> 
-      <Navigation/>
-
-    <div className="login-container">
-      <div className="login-box">
-        <h2>Login</h2>
-        <form>
-          <div className="textbox">
-            <input type="text" placeholder="Username/Email" />
+    <div className="page-container">
+      <Navigation />
+      <div className="login-container">
+        <div className="title-with-logo">
+          <h2 className="login-title">Login to SkinLens</h2>
+          <img src="/SkinLensLogo.jpg" alt="SkinLens Logo" className="skinlens-logo" />
+        </div>
+        <form className="login-form">
+          <div className="input-group">
+            <i className="fa fa-envelope input-icon"></i>
+            <input type="text" className="form-input" placeholder="Email" />
           </div>
 
-          <div className="textbox">
-            <input type="password" placeholder="Password" />
+          <div className="input-group">
+            <i className="fa fa-lock input-icon"></i>
+            <input type="password" className="form-input" placeholder="Password" />
           </div>
 
-          <input type="submit" className="btn" value="Login" />
+          <div className="form-footer">
+            <label className="remember-me">
+              <input type="checkbox" />
+              <span>Remember me</span> {/* Wrapped text in span for styling */}
+            </label>
+            <a href="/reset-password" className="forgot-password">Forgot password?</a>
+          </div>
+
+          <button type="submit" className="login-button">LOGIN</button>
         </form>
-
         <div className="signup-link">
-          <p>Don't have an account? <a href="/join">Sign up</a></p>
+          Don't have an account? <a href="/join" className="signup-button">Sign Up</a>
         </div>
       </div>
-    </div>
     </div>
   );
 }
