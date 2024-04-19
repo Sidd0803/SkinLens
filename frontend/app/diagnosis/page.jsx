6ad71a5f7cecb0ca0e68920ed989eef61d516aaa
hibakhatib <hibakhatib2025@u.northwestern.edu>
2024-04-19 14:48:09 -0500

    bootstrap installed

--- frontend/app/diagnosis/page.jsx
+++ frontend/app/diagnosis/page.jsx
@@ -1,147 +1,40 @@
 'use client'
-import React, { useEffect, useState } from 'react';
-import { ref, push } from 'firebase/database';
-import {database}  from '../firebase';
-import {
-    getDownloadURL,
-    ref as storageRef,
-    uploadBytes,
-  } from "firebase/storage";
+import React from "react";
+import Image from 'react-bootstrap/Image';
+import Card from 'react-bootstrap/Card';
+import { Button } from "react-bootstrap";
+import Form from 'react-bootstrap/Form';
 
-// ---- NEED TO HARD CODE A USER ID FOR NOW ----
+function DiagnosisPage(){
 
+    return(
 
-function DiagnosisPage() {
-    const [diagnosisResult, setDiagnosisResult] = useState('');
-    const [imageUpload, setImageUpload] = useState('');
-    const [userName, setUserName] = useState('');
+        <main>
 
+                <Card className="text-center">
+                
+                    <Card.Body>
+                        <Card.Title>Special title treatment</Card.Title>
+                        <Image src="banana.jpg"xs = {2} md = {2} xl = {1} width={200} height={200} rounded/> 
+                    </Card.Body>
+                </Card>
 
-    useEffect(() => {
-        const imagesRef = database.ref(database, 'users/ userid / images');
+                <Form.Group controlId="formFile" className="text-center">
+                    <Form.Label>Upload a photo for diagnosis</Form.Label>
+                    <Form.Control type="file" />
+                </Form.Group>
 
-        const imagesListener = imagesRef.on('child_added', (imagesRef, (snapshot) => {
-            const imageData = snapshot.val();
-            if (imageData) {
-                const { result } = imageData;
-                setDiagnosisResult(result);
-            }
-        }));
-    
-        return () => {
-            imagesListener();
-        };
-    }, []);
+                    <Card className = "text-center">
+                        <Card.Title>
+                            Your Results
+                            </Card.Title>
 
-    const handleUploadImage = () => {
-        if (imageUpload === null) {
-          toastifyError("Please select an image");
-          return;
-        }
-        const imageRef = ref(storage, 'user/userid /images}');
-    
-        uploadBytes(imageRef, imageUpload)
-          .then((snapshot) => {
-            getDownloadURL(snapshot.ref)
-              .then((url) => {
-                saveData(url);
-              })
-              .catch((error) => {
-                toastifyError(error.message);
-              });
-          })
-          .catch((error) => {
-            toastifyError(error.message);
-          });
-      };
-
-
-
-    
-    
-    return (
-        <div>
-            <h1>Welcome, {userName}!</h1>
-            <form onSubmit={handleUploadImage}>
-                <label htmlFor="Image">Select image:</label>
-                <input type="file" id="" name="Image" accept="image/*" onChange={(e) => {setImageUpload(e.target.files[0]);}} />
-                <input type="submit" value="Submit" />
-            </form>
-
-            <p>{diagnosisResult}</p>
-
-        </div>
-    );
+                        <Card.Body>
+                            Some data about your results go here!
+                        </Card.Body>
+                    </Card>
+        </main>
+    )
 }
 
-
-
-
-
 export default DiagnosisPage;
-
-
-
-//  old and bad handling of image upload to clean out // delete
-
-    // const handleFormSubmit = async (event) => {
-    //     event.preventDefault();
-    //     const formData = new FormData();
-    //     formData.append('image', event.target.elements.Image.files[0]);
-    
-    //     try {
-    //         const imageRef = push(ref(database, 'images'));
-    //         const snapshot = await uploadBytesResumable(imageRef, formData.get('image'));
-    //         console.log('Image uploaded successfully:', snapshot);
-    //     } catch (error) {
-    //         console.error('Error uploading image:', error);
-    //     }
-    // };
-
-
-
-//  need fetch methods for all types of data --> done
-
-
-
-   // useEffect(() => {
-    //     const generateFakeUserData = async () => {
-    //         const userRef = push(ref(database, 'users'));
-    //         const userData = {
-    //             email: 'user@example.com',
-    //             name: 'John Doe',
-    //             password: 'password123',
-    //             medications: ['Medication A', 'Medication B'],
-    //             physicians: [{
-    //                 name: 'Physician 1',
-    //                 rating: 4.5,
-    //                 specialty: 'Specialty A'
-    //             }],
-    //             logs: [{
-    //                 timestamp: new Date(),
-    //                 physician: 'Physician 1',
-    //                 message: 'Log message here'
-    //             }],
-    //             chats: [{
-    //                 chat_id: 'chat1',
-    //                 timestamp: new Date(),
-    //                 physician: 'Physician 1',
-    //                 message: 'Chat message here'
-    //             }],
-    //             data: [{
-    //                 timestamp: new Date(),
-    //                 personal_notes: 'Personal notes here',
-    //                 diagnosis: 'Diagnosis here',
-    //                 diagnosis_images: [{
-    //                     result: 'Diagnosis result',
-    //                     encoded_image_str: 'Encoded image string here'
-    //                 }]
-    //             }]
-    //         };
-
-    //         push(userRef, userData);
-    //         console.log('Generated data for user');
-    //     };
-
-    //     generateFakeUserData();
-    // }, []);
