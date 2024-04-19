8d68e992f8e87b0b374019db2bcd7cf510ffdc54
Hayah Ubaid <hayah.ubaid@gmail.com>
2024-04-19 17:02:31 -0500

    home page still in progress

--- frontend/app/home/page.jsx
+++ frontend/app/home/page.jsx
@@ -1,5 +1,12 @@
+'use client'
 import React from 'react';
 import Navigation from '../components/nav';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 
 function HomePage() {
@@ -10,39 +17,32 @@ function HomePage() {
     <main >
     <Navigation/>
  
-    <div className="container">
+    <div className="container" style={{ paddingTop: '20px' }}>
        
-        
-      <h1>Welcome to Your Skin Care Management App</h1>
-
-    <div>
-      <p>
-      Log New Skincare, Medication, or Progress <button className="log-button" href = "/log">Log</button>
-      </p>
-      </div>
-
-      <section>
-        <h2>Your Current Routine and Products</h2>
-        <ul>
-          {currentRoutine.map((item, index) => (
-            <li key={index}>
-              <strong>{item.product}</strong> - {item.timeOfDay}
-            </li>
-          ))}
-        </ul>
-      </section>
-
-      <section>
-        <h2>Your Most Recent Progress Pictures</h2>
-        <div className="progress-pictures">
-          {progressPictures.map((pic, index) => (
-            <div key={index} className="progress-picture">
-              <img src={pic.image} alt={`Progress ${index + 1}`} />
-              <p>Date: {pic.date}</p>
-            </div>
-          ))}
-        </div>
-      </section>
+        <Container>
+          <Row>
+            <Col xs = {3} md = {4} lg = {6}>
+            <h1>Hello, name {' '}
+              <Image src="SkinLensLogo.png" width = "100" height = "100" roundedCircle/>
+            </h1>
+            </Col>
+          </Row>
+        </Container>
+    </div>
+    <div style={{textAlign: 'center'}}>
+          <Button href="Log" size="lg" >Log</Button> {' '}
+          <Button as="input" type="submit" value="Contact Doctor" size="lg" />{' '}
+          <Button href="diagnosis" size="lg" >Get a Diagnosis</Button> 
+    </div>
+    <div style={{ paddingTop: '20px', textAlign: 'center' }}>
+      <Card className="text-center" style={{ width: '200px', height: '200px', margin: '0 auto'}}>
+        <Card.Body>
+          <Card.Title>Recent Doctor's Recommendations and Notes</Card.Title>
+           <Card.Text>
+          some recs and advice here
+          </Card.Text>
+        </Card.Body>
+    </Card>
     </div>
 
 
