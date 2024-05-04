84401d6db532102710d9574e87e5610118a359f8
RTB456 <159496952+RTB456@users.noreply.github.com>
2024-05-04 14:23:39 -0500

    log page

--- frontend/app/login/page.jsx
+++ frontend/app/login/page.jsx
@@ -1,81 +1,35 @@
 'use client'
-import React, { useState } from 'react';
+import React from 'react';
 import Navigation from '../components/nav';
-import styles from './page.modules.css'; // Assuming CSS Modules are being used correctly
-import { Stack, Button, Card } from 'react-bootstrap';
-
+import './page.modules.css'
 
 function LoginPage() {
-  const [showCards, setShowCards] = useState({
-    card1: false,
-    card2: false,
-    card3: false
-  });
-
-  const toggleCard = (cardKey) => {
-    setShowCards(prevState => ({
-      ...prevState,
-      [cardKey]: !prevState[cardKey]
-    }));
-  };
-
   return (
-    <div className={styles.pageContainer}> {/* Added CSS Module class here */}
+    <div> 
       <Navigation/>
-      <h1 className="titleStyle">My Data Logs</h1>
-      <h1 className="titleStyle2">Current Medications</h1>
-      <div className="p-2" style={{ marginBottom: '20px', minHeight: '200px' }}>  
-        {!showCards.card1 && (
-            <Button variant="primary" onClick={() => toggleCard('card1')}>Notes</Button>
-          )}
-          {showCards.card1 && (
-            <Card style={{ width: '24rem', marginTop: '10px', backgroundColor: '#84ACA9', display: showCards.card1 ? 'block' : 'none' }}>
-              <Card.Body>
-              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '175%'}}>
-              <Card.Title className="titleStyle3" style={{ flexShrink: 1, maxWidth: '85%' }}>Notes</Card.Title>
-                <Button
-                      variant="link"
-                      style={{
-                        padding: '0',
-                        marginRight: 'auto', 
-                        color: 'gray',
-                        fontSize: '1rem',
-                        backgroundColor: 'transparent',  // Ensures the background is transparent
-                        border: 'none',  // Removes any border
-                        textDecoration: 'none'  // Removes underline from the link
-                      }}
-                      onClick={() => toggleCard('card1')}
-                    >
-                    x
-                    </Button>
-                </div>
-                <textarea 
-                  className="form-control" // Bootstrap class for styling
-                  placeholder="Type your notes here..."
-                  rows="5" // Adjust the number of rows as needed
-                ></textarea>
-              </Card.Body>
-            </Card>
-          )}
+
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Login</h2>
+        <form>
+          <div className="textbox">
+            <input type="text" placeholder="Username/Email" />
+          </div>
+
+          <div className="textbox">
+            <input type="password" placeholder="Password" />
+          </div>
+
+          <input type="submit" className="btn" value="Login" />
+        </form>
+
+        <div className="signup-link">
+          <p>Don't have an account? <a href="/join">Sign up</a></p>
         </div>
-        <h1 className="titleStyle2">Current Skin Concerns</h1>
-        <div className="p-2" style={{ marginBottom: '20px', minHeight: '225px' }}>  
-        <Button variant="primary">add skin concern</Button>
-        </div>
+      </div>
+    </div>
     </div>
   );
 }
 
 export default LoginPage;
-
-
-{/* <Card style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title>Notes</Card.Title>
-          <textarea 
-          className="form-control" // Bootstrap class for styling
-          placeholder="Type your notes here..."
-          rows="5" // Adjust the number of rows as needed
-        ></textarea>
-        </Card.Body>
-      </Card>   */}
